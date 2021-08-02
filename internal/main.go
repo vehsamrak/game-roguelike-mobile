50464854ecf367edd174572079b513e94abddca7
Petr Karmashev <smonkl@bk.ru>
2021-08-02 21:50:16 +0300

    Image playaround

--- internal/main.go
+++ internal/main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
+	"image"
 	"log"
 	"os"
 
 	"gioui.org/app"
+	"gioui.org/f32"
 	"gioui.org/io/key"
 	"gioui.org/io/system"
+	"gioui.org/op"
+	"gioui.org/op/paint"
 	"gioui.org/unit"
 )
 
 func main() {
-	windowWidth := unit.Dp(100)
-	windowHeight := unit.Dp(100)
+	windowWidth := unit.Dp(600)
+	windowHeight := unit.Dp(1000)
 
 	go func() {
 		window := app.NewWindow(
@@ -30,7 +34,11 @@ func main() {
 }
 
 func run(window *app.Window) error {
-	// var ops op.Ops
+	img, _, _, err := loadImageFromFile("/home/petr/git/game-roguelike-mobile/Icon.png")
+	if err != nil {
+		return err
+	}
+
 	for {
 		windowEvent := <-window.Events()
 		switch event := windowEvent.(type) {
@@ -42,6 +50,35 @@ func run(window *app.Window) error {
 			case key.NameEscape:
 				return nil
 			}
+		case system.FrameEvent:
+			ops := new(op.Ops)
+			drawImage(ops, img)
+			event.Frame(ops)
+			// window.Invalidate()
 		}
 	}
+}
+
+func drawImage(ops *op.Ops, img image.Image) {
+	imageOp := paint.NewImageOp(img)
+	imageOp.Add(ops)
+	op.Affine(f32.Affine2D{}.Scale(f32.Pt(0, 0), f32.Pt(4, 4)))
+	paint.PaintOp{}.Add(ops)
+}
+
+func loadImageFromFile(filePath string) (img image.Image, height int, width int, err error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, 0, 0, err
+	}
+	defer file.Close()
+
+	img, _, err = image.Decode(file)
+
+	// imageConfig, _, err := image.DecodeConfig(file)
+	// if err != nil {
+	// 	return nil, 0, 0, err
+	// }
+
+	return img, 0, 0, err
 }
